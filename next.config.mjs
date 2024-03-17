80b969a193b4b43e27b67c076638d88ee18f15bc
Asiamah Jude <asiamah.jude@cellulant.io>
2024-03-17 16:15:16 +0300

    Intial COmmit

--- next.config.mjs
+++ next.config.mjs
@@ -4,30 +4,13 @@ const nextConfig = {
     NEXT_PUBLIC_CMS_API_URL: "https://site-api.datocms.com",
     NEXT_PUBLIC_CMS_API_TOKEN: "51f39259855732db02d00699f41740",
     NEXT_PUBLIC_CONTENTFUL_SPACE_ID: "373i90l99opx",
-    NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN: "4E_Cf6r5RsY9CCkMQW7xd6eBmu-jDEqBhzDjc5-38PQ",
-    NEXT_PUBLIC_CONTENTFUL_PREVIEW_ACCESS_TOKEN: "gca84TzMut1iGXe82GOhyqh7UDecucuO-Kw1D2I7sL8",
+    NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN:
+      "4E_Cf6r5RsY9CCkMQW7xd6eBmu-jDEqBhzDjc5-38PQ",
+    NEXT_PUBLIC_CONTENTFUL_PREVIEW_ACCESS_TOKEN:
+      "gca84TzMut1iGXe82GOhyqh7UDecucuO-Kw1D2I7sL8",
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "unsplash.com",
-        port: "",
-        // pathname: "/account123/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.ctfassets.net",
-        port: "",
-        // pathname: "/account123/**",
-      },
-      {
-        protocol: "https",
-        hostname: "media.licdn.com",
-        port: "",
-        // pathname: "/account123/**",
-      },
-    ],
+    domains: ["unsplash.com", "images.ctfassets.net", "media.licdn.com"],
   },
 };
 
